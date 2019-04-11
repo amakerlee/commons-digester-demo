cff1cc362813e5fff746415ab754e257956f7a0c
lintq <lintq@bbxpc.com>
2019-04-11 17:22:01 +0800

    异步解析XML示例

--- src/main/java/tqlin/AsyncParseMain.java
+++ src/main/java/tqlin/AsyncParseMain.java
@@ -2,23 +2,25 @@ package tqlin;
 
 import org.apache.commons.digester3.Digester;
 import org.apache.commons.digester3.binder.DigesterLoader;
-import org.xml.sax.SAXException;
 import tqlin.entity.Address;
 import tqlin.entity.Employee;
 
-import java.io.IOException;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.Future;
 
 /**
- * 规则模块预先绑定(RulesModule接口)示例
+ * 异步解析XML示例
  */
-public class DigesterLoaderMain {
+public class AsyncParseMain {
     private static DigesterLoader dl = DigesterLoader.newLoader(new EmployeeModule())
-            .setNamespaceAware(false);
+            .setNamespaceAware(false).setExecutorService(Executors.newSingleThreadExecutor());
 
-    public static void main(String[] args) throws IOException, SAXException {
+    public static void main(String[] args) throws ExecutionException, InterruptedException {
         Digester digester = dl.newDigester();
-        Employee employee = digester.parse(DigesterLoaderMain.class.getClassLoader().getResourceAsStream("employee.xml"));
 
+        Future<Employee> future = digester.asyncParse(AsyncParseMain.class.getClassLoader().getResourceAsStream("employee.xml"));
+        Employee employee = future.get();
         System.out.print(employee.getFirstName() + " ");
         System.out.print(employee.getLastName() + ", ");
         for (Address a : employee.getAddressList()) {
